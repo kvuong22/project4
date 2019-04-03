9af998a935fc91b3e474c8d0edacaedc7398e7a2
Kim Vuong <kvuong@umich.edu>
2019-04-03 16:58:53 -0400

    wrote code for writing to csv file so that it does not split at commas into new columns

--- csvfile.py
+++ csvfile.py
@@ -3,11 +3,21 @@ import csv
 
 
 
-outfile = open('national_parks.csv','w',encoding='utf-8')
-outfile.write('Name,Type,Description,Location')
-outfile.write('\n')
+# outfile = open('national_parks.csv','w',encoding='utf-8')
+# outfile.write('Name,Type,Description,Location')
+# outfile.write('\n')
+# for list in all_list:
+#     row_str = '{},{},{},{}'.format(list[0],list[1],list[2].replace('\n',' ').replace(',',' '),list[3].replace(',',' '))
+#     outfile.write(row_str)
+#     outfile.write('\n')
+# outfile.close()
+
+csvfile = open('national_parks.csv','w',newline='',encoding='utf-8')
+header = ('Name','Type','Description','Location')
+row_writer = csv.writer(csvfile)
+row_writer.writerow(header)
+
 for list in all_list:
-    row_str = '{},{},{},{}'.format(list[0],list[1],list[2].replace('\n',' ').replace(',',' '),list[3].replace(',',' '))
-    outfile.write(row_str)
-    outfile.write('\n')
-outfile.close()
+    row_writer.writerow(list)
+
+csvfile.close()
