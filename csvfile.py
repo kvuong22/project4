2003cb21c95434d8641143afda061c5cd46ed400
Kim Vuong <kvuong@umich.edu>
2019-04-03 23:19:04 -0400

    cleaned up comments in file

--- csvfile.py
+++ csvfile.py
@@ -1,17 +1,6 @@
 from SI507_project4 import *
 import csv
 
-
-
-# outfile = open('national_parks.csv','w',encoding='utf-8')
-# outfile.write('Name,Type,Description,Location')
-# outfile.write('\n')
-# for list in all_list:
-#     row_str = '{},{},{},{}'.format(list[0],list[1],list[2].replace('\n',' ').replace(',',' '),list[3].replace(',',' '))
-#     outfile.write(row_str)
-#     outfile.write('\n')
-# outfile.close()
-
 csvfile = open('national_parks.csv','w',newline='',encoding='utf-8')
 header = ('Name','Type','Description','Location')
 row_writer = csv.writer(csvfile)
